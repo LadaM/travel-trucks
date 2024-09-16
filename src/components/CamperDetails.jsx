8eb7b8598c4d411676938550b923b9feddb39fca
Lada <lada11lada@gmail.com>
2024-09-16 17:22:08 +0200

    navigation style

--- src/components/CamperDetails.jsx
+++ src/components/CamperDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCamperDetails } from '../store/camperDetailsSlice';
 import { useParams } from 'react-router-dom';
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import { NavLink } from 'react-router-dom';
+import css from './Navbar.module.css';
+import logo from '../assets/TravelTrucks.svg';
+import clsx from 'clsx';
+
+const getNavLinkClass = (isActive) => {
+  return clsx(css.navLink, isActive && css.active);
+};
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/catalog">Catalog</NavLink></li>
+    <nav className={css.navbar}>
+      <img src={logo} alt="TravelTrucks logo" />
+      <ul className={css.navLinkContainer}>
+        <li><NavLink to="/" className={({ isActive }) => getNavLinkClass(isActive)}>Home</NavLink></li>
+        <li><NavLink to="/catalog" className={({ isActive }) => getNavLinkClass(isActive)}>Catalog</NavLink></li>
       </ul>
     </nav>
   );
