86d81cfc53db1ffe962b66d58877e13d3703a7b7
Lada <lada11lada@gmail.com>
2024-09-16 17:47:10 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CamperList from './components/CampersList';
 import CamperDetails from './components/CamperDetails.jsx';
+import HomePage from './pages/HomePage.jsx';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CamperList />} />
         <Route path="/catalog/:id" element={<CamperDetails />} />
       </Routes>
